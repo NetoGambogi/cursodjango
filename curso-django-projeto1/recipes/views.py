e3a9b402637133128c178c34449b1faf9343e0c6
NetoGambogi <netogambogi@hotmail.com>
2026-03-29 17:39:53 -0300

    preparação e contextualização antes do projeto

---
+++ curso-django-projeto1/recipes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'recipes/home.html', context={'name': 'neto'})
+
+def sobre(request):
+    return render(request, 'recipes/sobre.html')
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
