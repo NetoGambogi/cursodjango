62738daa5f22174aa99adc9ce9f1deef3d4792e8
NetoGambogi <netogambogi@hotmail.com>
2026-04-03 16:35:38 -0300

    primeiro projeto básico, django static files

--- curso-django-projeto1/recipes/views.py
+++ curso-django-projeto1/recipes/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'neto'})
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
+    return render(request, 'recipes/pages/home.html', context={'name': 'neto'})
